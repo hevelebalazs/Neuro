67b24750713cc03520d2a2729e0823b4d7b71a92
hidden1 <hevele.balazs@gmail.com>
2025-11-16 13:30:30 +0100

    Initialized DirectX11

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,8 @@
 #define UNICODE
 #include <windows.h>
+#include <d3d11_1.h>
+
+#pragma comment(lib, "d3d11.lib")
 
 #define Assert(condition) { if(!(condition)) { DebugBreak(); } }
 
@@ -56,6 +59,85 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int) {
 		return GetLastError();
 	}
 
+	ID3D11Device *device = 0;
+	ID3D11DeviceContext *device_context = 0;
+	{
+		ID3D11Device *device0 = 0;
+		ID3D11DeviceContext *device_context0 = 0;
+		D3D_FEATURE_LEVEL feature_levels[] = { D3D_FEATURE_LEVEL_11_0 };
+		UINT creation_flags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+
+		HRESULT result = D3D11CreateDevice(0, D3D_DRIVER_TYPE_HARDWARE,
+										   0, creation_flags,
+										   feature_levels, ARRAYSIZE(feature_levels),
+										   D3D11_SDK_VERSION, &device0,
+										   0, &device_context0);
+
+		if(FAILED(result)) {
+			Assert(false);
+			return GetLastError();
+		}
+
+		result = device0->QueryInterface(__uuidof(ID3D11Device1), (void **)&device);
+		Assert(SUCCEEDED(result));
+		device0->Release();
+
+		result = device_context0->QueryInterface(__uuidof(ID3D11DeviceContext1), (void **)&device_context);
+		Assert(SUCCEEDED(result));
+		device_context0->Release();
+	}
+
+	IDXGISwapChain1 *swap_chain = 0;
+	{
+		IDXGIFactory2 *dxgi_factory = 0;
+		{
+			IDXGIDevice1 *dxgi_device = 0;
+			HRESULT result = device->QueryInterface(__uuidof(IDXGIDevice1), (void **)&dxgi_device);
+			Assert(SUCCEEDED(result));
+
+			IDXGIAdapter *dxgi_adapter = 0;
+			result = dxgi_device->GetAdapter(&dxgi_adapter);
+			Assert(SUCCEEDED(result));
+			dxgi_device->Release();
+
+			DXGI_ADAPTER_DESC adapter_desc = {};
+			dxgi_adapter->GetDesc(&adapter_desc);
+
+			result = dxgi_adapter->GetParent(__uuidof(IDXGIFactory2), (void **)&dxgi_factory);
+			Assert(SUCCEEDED(result));
+			dxgi_adapter->Release();
+		}
+
+		DXGI_SWAP_CHAIN_DESC1 swap_chain_desc = {};
+		swap_chain_desc.Width = 0;
+		swap_chain_desc.Height = 0;
+		swap_chain_desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM_SRGB;
+		swap_chain_desc.SampleDesc.Count = 1;
+		swap_chain_desc.SampleDesc.Quality = 0;
+		swap_chain_desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		swap_chain_desc.BufferCount = 2;
+		swap_chain_desc.Scaling = DXGI_SCALING_STRETCH;
+		swap_chain_desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+		swap_chain_desc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+		swap_chain_desc.Flags = 0;
+
+		HRESULT result = dxgi_factory->CreateSwapChainForHwnd(device, window, &swap_chain_desc, 0, 0, &swap_chain);
+		Assert(SUCCEEDED(result));
+
+		dxgi_factory->Release();
+	}
+
+	ID3D11RenderTargetView *frame_buffer_view = 0;
+	{
+		ID3D11Texture2D *frame_buffer = 0;
+		HRESULT result = swap_chain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void **)&frame_buffer);
+		Assert(SUCCEEDED(result));
+
+		result = device->CreateRenderTargetView(frame_buffer, 0, &frame_buffer_view);
+		Assert(SUCCEEDED(result));
+		frame_buffer->Release();
+	}
+
 	bool running = true;
 	while(running) {
 		MSG message = {};
@@ -68,7 +150,10 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int) {
 			DispatchMessageW(&message);
 		}
 
-		Sleep(1);
+		float background_color[4] = { 0.1f, 0.2f, 0.6f, 1.0f };
+		device_context->ClearRenderTargetView(frame_buffer_view, background_color);
+
+		swap_chain->Present(1, 0);
 	}
 
 	return 0;
